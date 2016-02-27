5c0be19aae2dfea9452602bc2d962daea549e862
yanbingbing <me@yanbingbing.com>
2016-02-27 16:56:34 +0800

    support dynamic width

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require('express');
+const stringWidth = require('string-width');
 
 const app = express();
 
@@ -8,50 +9,45 @@ const server = require('http').createServer(app);
 
 app.get('/', function (req, res) {
     res.type('image/svg+xml');
-    res.send(getsvg(req.query.title || 'hello', req.query.color, req.query.label, req.query.flat, req.query.labelColor));
+    let query = req.query;
+    res.send(getsvg(query.t || query.progress || query.title || 'hello', query.c || query.color, query.s || query.subject || query.label, query.f || query.flat, query.sc || query.subjectColor || query.labelColor));
     res.end();
 });
 
-function getsvg(title, color, label, flat, labelColor) {
+const CONFIG = {
+    height: 20,
+    progressBarWidth: 80,
+    fontSize: 12,
+    fontFamily: "Menlo, Lucida Console, monospace"
+};
+
+function getsvg(title, titleColor, subject, flat, subjectColor) {
     title = title || '';
-    label = label || '';
-    color = color || '#555';
-    labelColor = labelColor || '#162e34';
+    subject = subject || '';
+    titleColor = titleColor || '#555';
+    subjectColor = subjectColor || '#162e34';
 
-    let lM = 0,
-        lh = 40,
-        lH = 0,
-        lx = lh / 2,
-        width = 80,
-        h = width,
-        height = 20,
-        M = 0,
-        H = 0,
-        v = height,
-        x = width / 2,
-        ph = 0,
+    let progressWidth = 0,
         progress = false,
-        pcolor = '';
-
-    if (label) {
-        M = lh;
-        H = lh;
-        width += lh;
-        x += lh;
-    }
+        progressColor = '';
 
     if (/^\d+$/.test(title)) {
         progress = parseInt(title);
         title = title + '%';
-        ph = parseInt(0.8 * progress);
+        progressWidth = parseInt(CONFIG.progressBarWidth * progress / 100);
         if (progress < 30) {
-            pcolor = '#d9534f'
+            progressColor = '#d9534f'
         } else if (progress < 70) {
-            pcolor = '#f0ad4e';
+            progressColor = '#f0ad4e';
         } else {
-            pcolor = '#5cb85c';
+            progressColor = '#5cb85c';
         }
     }
+
+    let titleWidth = progress !== false ? CONFIG.progressBarWidth : (parseInt(stringWidth(title) * CONFIG.fontSize * 0.6) + 10);
+    let subjectWidth = subject ? parseInt(stringWidth(subject) * CONFIG.fontSize * 0.6 + 10) : 0;
+    let width = titleWidth + subjectWidth;
+    let height = CONFIG.height;
 
     let svg = [
         '<svg xmlns="http://www.w3.org/2000/svg" width="'+width+'" height="'+height+'">'
@@ -60,29 +56,36 @@ function getsvg(title, color, label, flat, labelColor) {
         svg.push('<linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient>');
         svg.push('<mask id="a"><rect width="'+width+'" height="'+height+'" rx="3" fill="#fff"/></mask>');
     }
-    svg.push('<g' + (flat ? '' : ' mask="url(#a)"') + '>');
+    svg.push('<g' + (flat ? '' : ' mask="url(#a)" ') + '>');
 
-    if (label) {
-        svg.push('<path fill="' + labelColor + '" d="M0 0h'+lh+'v'+v+'H0z"/>');
+    if (subject) {
+        svg.push('<path fill="' + subjectColor + '" d="M0 0h'+subjectWidth+'v'+height+'H0z"/>');
     }
 
-    svg.push('<path fill="'+color+'" d="M'+M+' 0h'+h+'v'+v+'H'+H+'z"/>');
+    svg.push('<path fill="'+titleColor+'" d="M'+subjectWidth+' 0h'+titleWidth+'v'+height+'H'+subjectWidth+'z"/>');
 
     if (progress !== false) {
-        svg.push('<path fill="'+pcolor+'" d="M'+M+' 0h'+ph+'v'+v+'H'+H+'z"/>');
+        svg.push('<path fill="'+progressColor+'" d="M'+subjectWidth+' 0h'+progressWidth+'v'+height+'H'+subjectWidth+'z"/>');
     }
 
     if (!flat) {
         svg.push('<path fill="url(#b)" d="M0 0h'+width+'v'+height+'H0z"/>');
     }
 
-    svg.push('</g><g fill="#fff" text-anchor="middle" font-family="Menlo, Lucida Console, monospace" font-size="12">');
+    svg.push('</g><g fill="#fff" text-anchor="middle" font-family="'+CONFIG.fontFamily+'" font-size="'+CONFIG.fontSize+'">');
 
-    if (label) {
-        svg.push('<text x="'+lx+'" y="14">'+label+'</text>');
+    if (subject) {
+        if (!flat) {
+            svg.push('<text x="'+(subjectWidth / 2)+'" fill="#010101" fill-opacity=".3" y="14">'+subject+'</text>');
+        }
+        svg.push('<text x="'+(subjectWidth / 2)+'" y="14">'+subject+'</text>');
+
     }
 
-    svg.push('<text x="'+x+'" y="14">'+title+'</text>');
+    if (!flat) {
+        svg.push('<text x="'+(subjectWidth + (titleWidth / 2))+'" fill="#010101" fill-opacity=".3" y="14">'+title+'</text>');
+    }
+    svg.push('<text x="'+(subjectWidth + (titleWidth / 2))+'" y="14">'+title+'</text>');
 
     svg.push('</g></svg>');
 
